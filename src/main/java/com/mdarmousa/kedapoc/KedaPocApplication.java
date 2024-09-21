91071ab068d4863e73a80359c59a191593d8a00a
Mohammad Darmousa <mdarmousa1@gmail.com>
2024-09-21 16:32:10 +0400

    add files

---
+++ src/main/java/com/mdarmousa/kedapoc/KedaPocApplication.java
@@ -0,0 +1,13 @@
+package com.mdarmousa.kedapoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KedaPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KedaPocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mdarmousa/kedapoc/KedaPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdarmousa.kedapoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KedaPocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
