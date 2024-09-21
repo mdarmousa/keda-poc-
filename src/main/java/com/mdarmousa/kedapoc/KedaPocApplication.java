898f006708f8317456d541f3fb747dc0c50208be
Mohammad Darmousa <mdarmousa1@gmail.com>
2024-09-21 22:09:09 +0400

    Add prometheus mangagement endpoint

--- src/main/java/com/mdarmousa/kedapoc/KedaPocApplication.java
+++ src/main/java/com/mdarmousa/kedapoc/KedaPocApplication.java
@@ -3,8 +3,13 @@ package com.mdarmousa.kedapoc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+import java.util.Map;
 
 @SpringBootApplication
 @RestController
@@ -17,6 +22,13 @@ public class KedaPocApplication {
     @GetMapping("/health")
     public Mono<String> health(){
         return Mono.just("Healthy");
+    }
+
+    @GetMapping("count/{count}")
+    public Mono<Map<String, String>> count(@PathVariable int count){
+        return Mono.just(Map.of("count", String.valueOf(count)))
+                .delayElement(Duration.ofSeconds(count));
+
     }
 
 }
