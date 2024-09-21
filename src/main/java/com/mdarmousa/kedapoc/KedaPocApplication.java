201c8e844acee089f472b61b7d1d96c2eed54a9d
Mohammad Darmousa <mdarmousa1@gmail.com>
2024-09-21 18:25:43 +0400

    update helth endpoint

--- src/main/java/com/mdarmousa/kedapoc/KedaPocApplication.java
+++ src/main/java/com/mdarmousa/kedapoc/KedaPocApplication.java
@@ -2,12 +2,21 @@ package com.mdarmousa.kedapoc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
+@RestController
 public class KedaPocApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(KedaPocApplication.class, args);
+    }
+
+    @GetMapping("/health")
+    public Mono<String> health(){
+        return Mono.just("Healthy");
     }
 
 }
